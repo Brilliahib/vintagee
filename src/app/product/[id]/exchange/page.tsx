dee6d872805f975f0180eed6deebe63982fe0dfe
brilliahib <brilliahib21@gmail.com>
2025-03-04 21:45:41 +0700

    feat: create exchange product

--- src/app/product/[id]/exchange/page.tsx
+++ src/app/product/[id]/exchange/page.tsx
@@ -1,8 +1,14 @@
 import Footer from "@/components/atoms/footer/Footer";
 import SectionTitle from "@/components/atoms/typography/SectionTitle";
 import Navbar from "@/components/organisms/navbar/Navbar";
+import ExchangeProductWrapper from "@/components/organisms/product/exchange/ExchangeProductWrapper";
 
-export default function ExchangePage() {
+interface ExchangePageParams {
+  params: Promise<{ id: string }>;
+}
+
+export default async function ExchangePage({ params }: ExchangePageParams) {
+  const { id } = await params;
   return (
     <>
       <Navbar />
@@ -10,6 +16,7 @@ export default function ExchangePage() {
         head="Tukar Fashion"
         body="Tukar fashion yang anda inginkan dengan produk yang anda punya."
       />
+      <ExchangeProductWrapper id={id} />
       <Footer />
     </>
   );
--- src/components/organisms/product/exchange/ExchangeProductWrapper.tsx
+++ src/components/organisms/product/exchange/ExchangeProductWrapper.tsx
@@ -1,7 +1,23 @@
-export default function ExchangeProductWrapper() {
+import PageContainer from "@/components/atoms/container/PageContainer";
+import ExchangeCreateContent from "./ExchangeCreateContent";
+import ExchangeDetailProductContent from "./ExchangeDetailProductContent";
+
+interface ExchangeProductWrapperProps {
+  id: string;
+}
+
+export default function ExchangeProductWrapper({
+  id,
+}: ExchangeProductWrapperProps) {
   return (
-    <div>
-      <div></div>
-    </div>
+    <PageContainer>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-6">
+        <div className="md:col-span-2">
+          <ExchangeCreateContent id={id} />
+        </div>
+
+        <ExchangeDetailProductContent id={id} />
+      </div>
+    </PageContainer>
   );
 }
